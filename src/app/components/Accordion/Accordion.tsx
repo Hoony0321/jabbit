3d54842a20095391ffd7050a5f91d33f9e22360b
Hoony0321 <eogns0321@gmail.com>
2025-02-14 03:58:59 +0000

    config : build.sh 추가

--- src/app/components/Accordion/Accordion.tsx
+++ src/app/components/Accordion/Accordion.tsx
@@ -1,5 +1,5 @@
 import { Flex, Text } from '@chakra-ui/react';
-import AccordionItemType from '../../types/accordionItem';
+import AccordionItemType from '../../../types/accordionItem';
 import {
   AccordionItem,
   AccordionItemContent,
@@ -16,18 +16,16 @@ const Accordion = ({ title, content }: AccordionProps) => {
   if (title) {
     return (
       <Flex flexDirection="column" width="1280px" marginTop="60px">
-        <Text fontSize="45px" fontWeight="600">
-          {title}
-        </Text>
+        <Text textStyle="xxl">{title}</Text>
         <AccordionRoot multiple>
           {content.map((item, index) => (
             <AccordionItem key={index} value={item.value}>
-              <AccordionItemTrigger fontSize="28px" padding="44px 0">
+              <AccordionItemTrigger textStyle="xl" padding="44px 0">
                 {item.title}
               </AccordionItemTrigger>
               <AccordionItemContent
-                fontSize="23px"
-                color="#666"
+                textStyle="lg"
+                color="main.black_2"
                 paddingLeft="32px"
               >
                 {item.text}
